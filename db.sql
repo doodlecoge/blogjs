bf9bf83540d143d0361e4ff5b39d0915d0b0dadb
huaichao wang <hchwang001@gmail.com>
2014-12-16 18:31:32 +0800

    add photo and tag img

--- db.sql
+++ db.sql
@@ -14,20 +14,23 @@ CREATE TABLE users
 (
   username   VARCHAR(20) NOT NULL PRIMARY KEY,
   password   VARCHAR(20) NOT NULL,
-  fullname   VARCHAR(20) NOT NULL,
+  fullname   VARCHAR(20) NULL,
+  photo      VARCHAR(24) NULL, -- <username> "." <ext>
+  role       VARCHAR(8)  NOT NULL DEFAULT 'user', -- "user" | "admin"
   created_at TIMESTAMP   NOT NULL
 );
 
 INSERT INTO users
-(username, password, fullname, created_at)
+(username, password, fullname, photo, role, created_at)
 VALUES
-  ('huaichao', 'pass1234', 'Huaichao Wang', '2014-11-29'),
-  ('chaochao', 'pass1234', 'Huaichao Wang', '2014-11-29');
+  ('huaichao', 'pass1234', 'Huaichao Wang', NULL, 'admin', '2014-11-29'),
+  ('chaochao', 'pass1234', 'Chaochao Wang', NULL, 'user', '2014-11-29');
 
 CREATE TABLE tags
 (
   id   INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(20) NOT NULL UNIQUE
+  name VARCHAR(20) NOT NULL UNIQUE,
+  img  VARCHAR(24) NULL -- <name> "." <ext>
 );
 
 INSERT INTO tags (id, name)
@@ -57,7 +60,7 @@ INSERT INTO articles
 (id, title, content, username, created_at, updated_at)
 VALUES
   (1, 'spring security', '', 'huaichao', '2014-11-29', '2014-11-29'),
-  (2, 'c/c++          ', '', 'huaichao', '2014-11-29', '2014-11-29'),
+  (2, 'c/c++          ', '', 'chaochao', '2014-11-29', '2014-11-29'),
   (3, 'sed command    ', '', 'huaichao', '2014-11-29', '2014-11-29'),
   (4, 'java reflection', '', 'huaichao', '2014-11-29', '2014-11-29'),
   (5, 'python network ', '', 'huaichao', '2014-11-29', '2014-11-29');
@@ -67,8 +70,10 @@ CREATE TABLE articles_tags
   aid INT NOT NULL,
   tid INT NOT NULL,
   PRIMARY KEY (aid, tid),
-  FOREIGN KEY (aid) REFERENCES articles (id),
+  FOREIGN KEY (aid) REFERENCES articles (id)
+    ON DELETE CASCADE,
   FOREIGN KEY (tid) REFERENCES tags (id)
+    ON DELETE CASCADE
 );
 
 INSERT INTO articles_tags
